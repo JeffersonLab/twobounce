f825c4b3388e24d8bf9638a5e835620bcbb90e2a
Cameron Clarke <cameronc137@gmail.com>
2018-11-30 16:11:33 -0500

    Made a bunch of additions See: https://kk-router.physics.sunysb.edu/Radiation,+Shielding,+and+Configuration+Simulations/57

--- speccoll_cameron.py
+++ speccoll_cameron.py
@@ -12,31 +12,113 @@ allpolys = []
 tgtlen = 1.5
 #tgtrad = 0.04
 #tgtrad = 0.002
-tgtrad = 0.0025
+tgtrad = 1.414*0.0025
 
 target = polygon( ([-tgtlen/2,-tgtrad], [tgtlen/2, -tgtrad], [tgtlen/2,tgtrad], [-tgtlen/2,tgtrad]) )
 
+################################################################
+
+###### Lead collar (for collimating photons)
+
+#seg 1
+x1_collar=0.074
+z1_collar=2.75
+
+x2_collar=0.185 #*1.414 The collar is a rectangle (in CAD), but the narrow dimension is the one relevant for here
+z2_collar=2.75
+
+x3_collar=0.185 #*1.414
+z3_collar=2.95
+
+x4_collar=0.074
+z4_collar=2.95
+
+
+collar_top    = polygon( ([z1_collar,  x1_collar], [z4_collar,  x4_collar], [z3_collar,  x3_collar], [z2_collar,  x2_collar] ), notSource=False)
+collar_bottom = polygon( ([z2_collar, -x2_collar], [z3_collar, -x3_collar], [z4_collar, -x4_collar], [z1_collar, -x1_collar] ), notSource=False)
 
 #################################################################
 
-######inner photon collimator
+######inner photon collimator (Col 1)
+# Previous implementation assumed it was a solid shape from front face to back face, 
+# but this neglects the fact that it tapers inwards a lot in the first 30cm and then 
+# untapers in the last 10cm - updating by splitting into 5 segments
 
-x1_inner_photon=0.024
-z1_inner_photon=5.175
+x1_inner_photon_1=0.024
+z1_inner_photon_1=5.175
 
-x2_inner_photon=0.0317
-z2_inner_photon=5.175
+x2_inner_photon_1=0.031703 # 0.05670 is the outer radius of the Col1 cooling fans, 0.031703 is the outer radius of Col1 otherwise
+z2_inner_photon_1=5.175
 
-x3_inner_photon=0.03175
-z3_inner_photon=5.7
+x3_inner_photon_1=0.031703
+z3_inner_photon_1=5.275
 
-x4_inner_photon=0.0165
-z4_inner_photon=5.7
+x4_inner_photon_1=0.024
+z4_inner_photon_1=5.275
+
+x1_inner_photon_2=0.020386
+z1_inner_photon_2=5.275
+
+x2_inner_photon_2=0.028703
+z2_inner_photon_2=5.275
+
+x3_inner_photon_2=0.028703
+z3_inner_photon_2=5.375
+
+x4_inner_photon_2=0.020386
+z4_inner_photon_2=5.375
+
+x1_inner_photon_3=0.018696
+z1_inner_photon_3=5.375
+
+x2_inner_photon_3=0.026703
+z2_inner_photon_3=5.375
+
+x3_inner_photon_3=0.026703
+z3_inner_photon_3=5.475
+
+x4_inner_photon_3=0.018696
+z4_inner_photon_3=5.475
+
+x1_inner_photon_4=0.015529
+z1_inner_photon_4=5.475
+
+x2_inner_photon_4=0.026703
+z2_inner_photon_4=5.475
+
+x3_inner_photon_4=0.026703
+z3_inner_photon_4=5.575
+
+x4_inner_photon_4=0.015529
+z4_inner_photon_4=5.575
+
+x1_inner_photon_5=0.015529
+z1_inner_photon_5=5.575
+
+x2_inner_photon_5=0.026703
+z2_inner_photon_5=5.575
+
+x3_inner_photon_5=0.026703
+z3_inner_photon_5=5.675
+
+x4_inner_photon_5=0.015808
+z4_inner_photon_5=5.675
 
 # one has to assign the coordinates in anti-clock sequence
-coll_inner_photon_1   = polygon( ([z1_inner_photon, x1_inner_photon], [z4_inner_photon, x4_inner_photon], [z3_inner_photon, x3_inner_photon], [z2_inner_photon, x2_inner_photon] ), notSource=False)
+coll_inner_photon_top_1    = polygon( ([z1_inner_photon_1,  x1_inner_photon_1], [z4_inner_photon_1,  x4_inner_photon_1], [z3_inner_photon_1,  x3_inner_photon_1], [z2_inner_photon_1,  x2_inner_photon_1] ), notSource=False)
+coll_inner_photon_bottom_1 = polygon( ([z2_inner_photon_1, -x2_inner_photon_1], [z3_inner_photon_1, -x3_inner_photon_1], [z4_inner_photon_1, -x4_inner_photon_1], [z1_inner_photon_1, -x1_inner_photon_1] ), notSource=False)
 
-coll_inner_photon_2 = polygon( ([z2_inner_photon, -x2_inner_photon], [z3_inner_photon, -x3_inner_photon], [z4_inner_photon, -x4_inner_photon], [z1_inner_photon, -x1_inner_photon] ), notSource=False)
+coll_inner_photon_top_2    = polygon( ([z1_inner_photon_2,  x1_inner_photon_2], [z4_inner_photon_2,  x4_inner_photon_2], [z3_inner_photon_2,  x3_inner_photon_2], [z2_inner_photon_2,  x2_inner_photon_2] ), notSource=False)
+coll_inner_photon_bottom_2 = polygon( ([z2_inner_photon_2, -x2_inner_photon_2], [z3_inner_photon_2, -x3_inner_photon_2], [z4_inner_photon_2, -x4_inner_photon_2], [z1_inner_photon_2, -x1_inner_photon_2] ), notSource=False)
+
+coll_inner_photon_top_3    = polygon( ([z1_inner_photon_3,  x1_inner_photon_3], [z4_inner_photon_3,  x4_inner_photon_3], [z3_inner_photon_3,  x3_inner_photon_3], [z2_inner_photon_3,  x2_inner_photon_3] ), notSource=False)
+coll_inner_photon_bottom_3 = polygon( ([z2_inner_photon_3, -x2_inner_photon_3], [z3_inner_photon_3, -x3_inner_photon_3], [z4_inner_photon_3, -x4_inner_photon_3], [z1_inner_photon_3, -x1_inner_photon_3] ), notSource=False)
+
+coll_inner_photon_top_4    = polygon( ([z1_inner_photon_4,  x1_inner_photon_4], [z4_inner_photon_4,  x4_inner_photon_4], [z3_inner_photon_4,  x3_inner_photon_4], [z2_inner_photon_4,  x2_inner_photon_4] ), notSource=False)
+coll_inner_photon_bottom_4 = polygon( ([z2_inner_photon_4, -x2_inner_photon_4], [z3_inner_photon_4, -x3_inner_photon_4], [z4_inner_photon_4, -x4_inner_photon_4], [z1_inner_photon_4, -x1_inner_photon_4] ), notSource=False)
+
+coll_inner_photon_top_5    = polygon( ([z1_inner_photon_5,  x1_inner_photon_5], [z4_inner_photon_5,  x4_inner_photon_5], [z3_inner_photon_5,  x3_inner_photon_5], [z2_inner_photon_5,  x2_inner_photon_5] ), notSource=False)
+coll_inner_photon_bottom_5 = polygon( ([z2_inner_photon_5, -x2_inner_photon_5], [z3_inner_photon_5, -x3_inner_photon_5], [z4_inner_photon_5, -x4_inner_photon_5], [z1_inner_photon_5, -x1_inner_photon_5] ), notSource=False)
 
 
 #########collimator 2,  three segments
@@ -44,7 +126,7 @@ coll_inner_photon_2 = polygon( ([z2_inner_photon, -x2_inner_photon], [z3_inner_p
 bigger=0*0.1515
 
 #seg 1
-x1_coll_2_1=-0.15 -bigger+ (-0.5)
+x1_coll_2_1=-0.30 -bigger
 z1_coll_2_1=5.875
 
 x2_coll_2_1=-0.098
@@ -53,7 +135,7 @@ z2_coll_2_1=5.875
 x3_coll_2_1=-0.098
 z3_coll_2_1=5.975
 
-x4_coll_2_1=-0.15 -bigger+(-0.5)
+x4_coll_2_1=-0.30 -bigger
 z4_coll_2_1=5.975
 
 
@@ -82,10 +164,10 @@ coll_2_2   = polygon( ([z1_coll_2_2, x1_coll_2_2], [z4_coll_2_2, x4_coll_2_2], [
 x1_coll_2_3=0.026
 z1_coll_2_3=5.875
 
-x2_coll_2_3=0.15 +bigger+0.5
+x2_coll_2_3=0.30 +bigger
 z2_coll_2_3=5.875
 
-x3_coll_2_3=0.15 +bigger+0.5
+x3_coll_2_3=0.30 +bigger
 z3_coll_2_3=5.975
 
 x4_coll_2_3=0.026
@@ -95,7 +177,7 @@ z4_coll_2_3=5.975
 coll_2_3   = polygon( ([z1_coll_2_3, x1_coll_2_3], [z4_coll_2_3, x4_coll_2_3], [z3_coll_2_3, x3_coll_2_3], [z2_coll_2_3, x2_coll_2_3] ), notSource=False)
 
 
-#############inner_pipe
+############# Col2 photon collimating inner_pipe
 
 x1_inner_pipe=0.0265
 z1_inner_pipe=5.975
@@ -116,21 +198,20 @@ coll_inner_pipe_2   = polygon( ([z2_inner_pipe, -x2_inner_pipe], [z3_inner_pipe,
 ################# collimator 4, 3 segmentations
 
 downstream_col4_shift=-1.5
-dss=downstream_col4_shift ### This shift is largely arbitrary FIXME (doesn't include the lead photon collimator Rakitha added)
-
+dss=downstream_col4_shift ### This shift is largely arbitrary
 
 #seg 1
 
-x1_coll_4_1=-0.254#Was simulation -0.300 # Was CAD -0.254
+x1_coll_4_1=-0.254 #Was simulation -0.300 # Was CAD -0.254
 z1_coll_4_1=9.775+dss
 
-x2_coll_4_1=-0.1655#Was -0.23495
+x2_coll_4_1=-0.1714 #Was -0.23495, then was -0.1655, current sculpt values reflect updated moller envelopes at this upstream z position
 z2_coll_4_1=9.775+dss
 
-x3_coll_4_1=-0.1655#Was -0.23495
+x3_coll_4_1=-0.1714 #Was -0.23495, then was -0.1655
 z3_coll_4_1=9.875+dss
 
-x4_coll_4_1=-0.254#Was simulation -0.300 # Was CAD -0.254
+x4_coll_4_1=-0.254 #Was simulation -0.300 # Was CAD -0.254
 z4_coll_4_1=9.875+dss
 
 
@@ -139,7 +220,7 @@ coll_4_1   = polygon( ([z1_coll_4_1, x1_coll_4_1], [z4_coll_4_1, x4_coll_4_1], [
 
 #seg 2
 
-x1_coll_4_2=-0.0525#Was -0.06377
+x1_coll_4_2=-0.05 #Was -0.06377, then was -0.0525
 z1_coll_4_2=9.775+dss
 
 x2_coll_4_2=-0.030
@@ -148,7 +229,7 @@ z2_coll_4_2=9.775+dss
 x3_coll_4_2=-0.030
 z3_coll_4_2=9.875+dss
 
-x4_coll_4_2=-0.0525#Was -0.06377
+x4_coll_4_2=-0.05 #Was -0.06377, then was -0.0525
 z4_coll_4_2=9.875+dss
 
 
@@ -159,10 +240,10 @@ coll_4_2   = polygon( ([z1_coll_4_2, x1_coll_4_2], [z4_coll_4_2, x4_coll_4_2], [
 x1_coll_4_3=0.030
 z1_coll_4_3=9.775+dss
 
-x2_coll_4_3=0.254#Was simulation -0.300 # Was CAD -0.254
+x2_coll_4_3=0.254 #Was simulation -0.300 # Was CAD -0.254
 z2_coll_4_3=9.775+dss
 
-x3_coll_4_3=0.254#Was simulation -0.300 # Was CAD -0.254
+x3_coll_4_3=0.254 #Was simulation -0.300 # Was CAD -0.254
 z3_coll_4_3=9.875+dss
 
 x4_coll_4_3=0.030
@@ -171,7 +252,7 @@ z4_coll_4_3=9.875+dss
 
 coll_4_3   = polygon( ([z1_coll_4_3, x1_coll_4_3], [z4_coll_4_3, x4_coll_4_3], [z3_coll_4_3, x3_coll_4_3], [z2_coll_4_3, x2_coll_4_3]), notSource=False)
 
-#############inner_pipe_2
+############# Col4 photon collimating inner_pipe_2
 
 x1_inner_pipe_2=0.033
 z1_inner_pipe_2=9.875+dss
@@ -184,7 +265,6 @@ z3_inner_pipe_2_tmp=11.450+dss
 
 x4_inner_pipe_2_tmp=0.0366
 z4_inner_pipe_2_tmp=11.450+dss
-## extend forward only the first bit of the pipe (ARBITRARILY FIXME)
 
 z3=11.45+0.4
 x3_inner_pipe_2=(x3_inner_pipe_2_tmp-x2_inner_pipe_2)/(z3_inner_pipe_2_tmp-z2_inner_pipe_2)*(z3-z2_inner_pipe_2)+x2_inner_pipe_2
@@ -199,8 +279,11 @@ coll_inner_pipe_2_1   = polygon( ([z1_inner_pipe_2, x1_inner_pipe_2], [z4_inner_
 coll_inner_pipe_2_2   = polygon( ([z2_inner_pipe_2, -x2_inner_pipe_2], [z3_inner_pipe_2, -x3_inner_pipe_2], [z4_inner_pipe_2, -x4_inner_pipe_2], [z1_inner_pipe_2, -x1_inner_pipe_2]), notSource=False)
 
 
-#########collimator 5, 1 seg
-x1_coll_5=-0.1055
+######### collimator 5 (shaped like a tuning fork), 1 seg
+
+#x1_coll_5=-0.1055 # use this if you just want the exact y=0 slice
+
+x1_coll_5=-0.1863 # use this if you want the maximum radial extent
 z1_coll_5=12.8
 
 x2_coll_5=-0.042
@@ -209,7 +292,9 @@ z2_coll_5=12.8
 x3_coll_5=-0.042
 z3_coll_5=12.87
 
-x4_coll_5=-0.1055
+#x4_coll_5=-0.1055 # use this if you just want the exact y=0 slice
+
+x4_coll_5=-0.1863 # use this if you want the maximum radial extent
 z4_coll_5=12.87
 
 
@@ -221,17 +306,45 @@ coll_5   = polygon( ([z1_coll_5, x1_coll_5], [z4_coll_5, x4_coll_5], [z3_coll_5,
 #quartz1 = polygon( ([28.0, 0.6], [28.01, 0.6], [28.01, 1.4], [28, 1.4]), isDetector=True )
 #quartz2 = polygon( ([28.0, -1.4], [28.01, -1.4], [28.01, -0.6], [28, -0.6]), isDetector=True )
 
-quartz1 = polygon( ([28.0, 0.55], [28.02, 0.55], [28.02, 1.3], [28, 1.3]), isDetector=True )
-quartz2 = polygon( ([28.0, -1.3], [28.02, -1.3], [28.02, -0.55], [28, -0.55]), isDetector=True )
+det_inner_radius=0.69 # was 0.55, probably fine at 0.6, ring 1 quartz actually begins at 0.69
+det_outer_radius=1.30 # ring 6 quartz ends at 1.2, PMTs begin at 1.3
+det_z_pos=28.0
+det_z_extent=1.0 # was = .02 for ideal detector
+
+quartz1 = polygon( ([det_z_pos, det_inner_radius], [det_z_pos+det_z_extent, det_inner_radius], [det_z_pos+det_z_extent, det_outer_radius], [det_z_pos, det_outer_radius]), isDetector=True )
+quartz2 = polygon( ([det_z_pos, -det_outer_radius], [det_z_pos+det_z_extent, -det_outer_radius], [det_z_pos+det_z_extent, -det_inner_radius], [det_z_pos, -det_inner_radius]), isDetector=True )
+
+#### sub-quartz array detector to give an idea about the available space before photons become an issue again
+
+sub_det_inner_radius=0.60 # was 0.55, probably fine at 0.6, ring 1 quartz actually begins at 0.69
+sub_det_outer_radius=0.689 # ring 6 quartz ends at 1.2, PMTs begin at 1.3
+sub_det_z_pos=28.0
+sub_det_z_extent=1.0 # was = .02 for ideal detector
+
+sub_quartz1 = polygon( ([sub_det_z_pos, sub_det_inner_radius], [sub_det_z_pos+sub_det_z_extent, sub_det_inner_radius], [sub_det_z_pos+sub_det_z_extent, sub_det_outer_radius], [sub_det_z_pos, sub_det_outer_radius]), isDetector=True )
+sub_quartz2 = polygon( ([sub_det_z_pos, -sub_det_outer_radius], [sub_det_z_pos+sub_det_z_extent, -sub_det_outer_radius], [sub_det_z_pos+sub_det_z_extent, -sub_det_inner_radius], [sub_det_z_pos, -sub_det_inner_radius]), isDetector=True )
+
+
+#quartz1 = polygon( ([28.0, 0.55], [28.02, 0.55], [28.02, 1.3], [28, 1.3]), isDetector=True )
+#quartz2 = polygon( ([28.0, -1.3], [28.02, -1.3], [28.02, -0.55], [28, -0.55]), isDetector=True )
 
 ##################################################################################################################################
 
 sources.append(target)
 
+allpolys.append(collar_top)
+allpolys.append(collar_bottom)
 
-
-allpolys.append(coll_inner_photon_1)
-allpolys.append(coll_inner_photon_2)
+allpolys.append(coll_inner_photon_top_1)
+allpolys.append(coll_inner_photon_bottom_1)
+allpolys.append(coll_inner_photon_top_2)
+allpolys.append(coll_inner_photon_bottom_2)
+allpolys.append(coll_inner_photon_top_3)
+allpolys.append(coll_inner_photon_bottom_3)
+allpolys.append(coll_inner_photon_top_4)
+allpolys.append(coll_inner_photon_bottom_4)
+allpolys.append(coll_inner_photon_top_5)
+allpolys.append(coll_inner_photon_bottom_5)
 
 allpolys.append(coll_2_1)
 allpolys.append(coll_2_2)
@@ -244,13 +357,15 @@ allpolys.append(coll_4_1)
 allpolys.append(coll_4_2)
 allpolys.append(coll_4_3)
 
-allpolys.append(coll_inner_pipe_2_1)
-allpolys.append(coll_inner_pipe_2_2)
+#allpolys.append(coll_inner_pipe_2_1)
+#allpolys.append(coll_inner_pipe_2_2)
 
 allpolys.append(coll_5)
 
 allpolys.append(quartz1)
 allpolys.append(quartz2)
+allpolys.append(sub_quartz1)
+allpolys.append(sub_quartz2)
 
 ########################################################################
 #  Rough out blocking areas
